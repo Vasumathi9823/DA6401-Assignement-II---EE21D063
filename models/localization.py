5f765f97e3e5664e0f367857081993b21690f1fd
Vasumathi Raman <103988914+Vasumathi9823@users.noreply.github.com>
2026-04-12 12:59:33 +0530

    Update localization.py

--- models/localization.py
+++ models/localization.py
@@ -1,29 +1,39 @@
-"""Localization modules
-"""
+"""Localization modules"""
 
 import torch
 import torch.nn as nn
+from .vgg11 import VGG11Encoder
+from .layers import CustomDropout
 
 class VGG11Localizer(nn.Module):
     """VGG11-based localizer."""
 
     def __init__(self, in_channels: int = 3, dropout_p: float = 0.5):
-        """
-        Initialize the VGG11Localizer model.
-
-        Args:
-            in_channels: Number of input channels.
-            dropout_p: Dropout probability for the localization head.
-        """
-        pass
+        """Initialize the VGG11Localizer model."""
+        super().__init__()
+        
+        self.encoder = VGG11Encoder(in_channels)
+        
+        # Regression head for bounding box prediction
+        self.regressor = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+            # Final output is exactly 4 continuous values
+            nn.Linear(4096, 4) 
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass for localization model.
-        Args:
-            x: Input tensor of shape [B, in_channels, H, W].
-
-        Returns:
-            Bounding box coordinates [B, 4] in (x_center, y_center, width, height) format in original image pixel space(not normalized values).
-        """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11Localizer.forward")
+        """Forward pass for localization model."""
+        # Extract features
+        x = self.encoder(x, return_features=False)
+        
+        # Flatten spatial dimensions
+        x = torch.flatten(x, 1)
+        
+        # Output coordinates
+        x = self.regressor(x)
+        return x
