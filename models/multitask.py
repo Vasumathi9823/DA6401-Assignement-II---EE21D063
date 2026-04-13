1d7db92e295669cdad7f11826ecb3f1bc3ab7818
Vasumathi Raman <103988914+Vasumathi9823@users.noreply.github.com>
2026-04-13 12:37:35 +0530

    Update multitask.py

--- models/multitask.py
+++ models/multitask.py
@@ -18,10 +18,11 @@ class MultiTaskPerceptionModel(nn.Module):
         """Initialize the shared backbone/heads using trained weights."""
         super().__init__()
         #https://drive.google.com/file/d/1484W3kYuCSE3MjaWNCDNuE3Uv-m86r2A/view?usp=sharing
+        # https://drive.google.com/file/d/1vd_kGzY756h6Yp3mL0HmcAPudAReCZA-/view?usp=sharing
         import gdown
         gdown.download(id="1484W3kYuCSE3MjaWNCDNuE3Uv-m86r2A", output=classifier_path, quiet=False)
         gdown.download(id="1Iglso8WczzJGmbSuod8xivhraXFN5i-d", output=localizer_path, quiet=False)
-        gdown.download(id="1N-FHY7BwzBh4Vw1n4TRQzZPD0nBFRxeW", output=unet_path, quiet=False)
+        gdown.download(id="1vd_kGzY756h6Yp3mL0HmcAPudAReCZA-", output=unet_path, quiet=False)
         
         # Shared Encoder
         self.shared_encoder = VGG11Encoder(in_channels)
