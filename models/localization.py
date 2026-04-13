137bee9bfdfbade05a9b9f19b30eb2d0068d5875
Vasumathi Raman <103988914+Vasumathi9823@users.noreply.github.com>
2026-04-13 15:26:17 +0530

    Update localization.py

--- models/localization.py
+++ models/localization.py
@@ -1,18 +1,12 @@
 """Localization modules"""
-
 import torch
 import torch.nn as nn
 from .vgg11 import VGG11Encoder
 
 class VGG11Localizer(nn.Module):
-    """VGG11-based localizer."""
-
     def __init__(self, in_channels: int = 3, dropout_p: float = 0.0):
-        """Initialize the VGG11Localizer model."""
         super().__init__()
-        
         self.encoder = VGG11Encoder(in_channels)
-        
         self.regressor = nn.Sequential(
             nn.Linear(512 * 7 * 7, 512),
             nn.ReLU(inplace=True),
@@ -22,10 +16,7 @@ class VGG11Localizer(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass for localization model."""
         x = self.encoder(x, return_features=False)
         x = torch.flatten(x, 1)
         x = self.regressor(x)
-        
-        # FIX: Force absolute pixel space [0, 224] for Gradescope!
         return torch.sigmoid(x) * 224.0
